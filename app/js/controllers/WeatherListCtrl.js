b05b81b3b055a28ce42b14ccbac928560d9d0d4e
nlogar <nlogar@lokacom.si>
2014-03-14 15:21:59 +0100

    Made some changes

--- app/js/controllers/WeatherListCtrl.js
+++ app/js/controllers/WeatherListCtrl.js
@@ -3,13 +3,15 @@
 /* Controllers */
 
 angular.module('weatherApp.controllers').
-  controller('WeatherListCtrl', ['$scope', 'weatherService', function($scope, weatherService) {
+    controller('WeatherListCtrl', ['$scope', 'weatherService', function ($scope, weatherService) {
         $scope.title = 'Your favorite weather reports';
         $scope.weatherDataList = [];
+        $scope.template = $("#template").html();
 
-        weatherService.getWeatherList().then(function (data) {
-            $scope.weatherDataList = data;
-        }, function (error) {
-
-        });
+        var weatherListPromise = weatherService.getWeatherList();
+        if (weatherListPromise) {
+            weatherListPromise.then(function (data) {
+                $scope.weatherDataList = new kendo.data.DataSource({ data: data });
+            });
+        }
     }]);
